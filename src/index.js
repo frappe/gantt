350ff7f0c3a232936db35db3c260b3755b30a7e2
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2024-09-11 21:05:20 +0530

    chore: format

--- src/index.js
+++ src/index.js
@@ -739,8 +739,7 @@ export default class Gantt {
             });
             $lower_text.innerText = date.lower_text;
             $lower_text.style.left =
-                +$lower_text.style.left.slice(0, -2) +
-                'px';
+                +$lower_text.style.left.slice(0, -2) + 'px';
 
             if (date.upper_text) {
                 this.upper_texts_x[date.upper_text] = date.upper_x;
