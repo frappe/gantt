1a09a38a6e3c1d2da725cd59b95ab6c8e0eb2d93
Praveen <uhasneevarp@gmail.com>
2025-01-14 10:48:53 +0530

    fix: on calling update_task, start and end date not being updated

--- src/bar.js
+++ src/bar.js
@@ -51,6 +51,8 @@ export default class Bar {
         this.invalid = this.task.invalid;
         this.height = this.gantt.options.bar_height;
         this.image_size = this.height - 5;
+        this.task._start = new Date(this.task.start);
+        this.task._end = new Date(this.task.end);
         this.compute_x();
         this.compute_y();
         this.compute_duration();
