2850787e72bac723c915fa955a351affb90d10df
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-01-16 11:55:20 +0530

    chore: bump version

--- src/index.js
+++ src/index.js
@@ -240,7 +240,7 @@ export default class Gantt {
         this.setup_dates(maintain_pos);
         this.render();
         if (maintain_pos) {
-            this.$container.scrollLeft =old_pos;
+            this.$container.scrollLeft = old_pos;
             this.options.scroll_to = old_scroll_op;
         }
         this.trigger_event('view_change', [mode]);
