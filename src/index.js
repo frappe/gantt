9a25b4fd0956ae8ebf603f7f5a42ddc07f04b2e4
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-02-17 15:57:12 +0530

    chore: format

--- src/index.js
+++ src/index.js
@@ -1459,7 +1459,9 @@ export default class Gantt {
             }, []);
 
             out = out.concat(deps);
-            to_process = deps.filter((d) => !to_process.includes(d) && !out.includes(d));
+            to_process = deps.filter(
+                (d) => !to_process.includes(d) && !out.includes(d),
+            );
         }
 
         return out.filter(Boolean);
