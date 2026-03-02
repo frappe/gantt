af3a2a288ecbf2e890ea58c02bfc8ab4f180b2dc
Tanmay Wankar <62742498+tanmaywankar@users.noreply.github.com>
2026-03-02 13:48:27 +0530

    fix: clamp bar_corner_radius to prevent ellipse distortion

--- src/bar.js
+++ src/bar.js
@@ -56,7 +56,7 @@ export default class Bar {
         this.compute_x();
         this.compute_y();
         this.compute_duration();
-        this.corner_radius = this.gantt.options.bar_corner_radius;
+        this.corner_radius = Math.min(this.gantt.options.bar_corner_radius, this.height/2);
         this.width = this.gantt.config.column_width * this.duration;
         if (!this.task.progress || this.task.progress < 0)
             this.task.progress = 0;
